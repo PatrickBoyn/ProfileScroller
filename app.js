521a7a907bc06eb34bd302b531b23e3171a4b0a2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-09 22:48:25 -0400

    Added the user data.

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    loocation: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+  {
+    name: 'Jane Doe',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    loocation: 'Pheonix AZ',
+    image: 'https://randomuser.me/api/portraits/women/2.jpg'
+  },
+  {
+    name: 'James Smith',
+    age: 22,
+    gender: 'male',
+    lookingfor: 'female',
+    loocation: 'Washington DC',
+    image: 'https://randomuser.me/api/portraits/men/25.jpg'
+  }
+];
